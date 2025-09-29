8d2127e4303c63eabbf1c018d7bf976cc51a0524
PeterSomkuti <peter.somkuti@gmail.com>
2025-09-29 15:18:31 -0400

    Doctests are now performed during testing.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,22 @@
 using RetrievalToolbox
+using Documenter
 using Test
 using Unitful
 
 @testset "RetrievalToolbox tests" begin
+
+    @testset "Doctests" begin
+
+        # Make sure module is loaded for all doctests
+        DocMeta.setdocmeta!(
+            RetrievalToolbox,
+            :DocTestSetup,
+            :(using RetrievalToolbox);
+            recursive=true
+        )
+
+        doctest(RetrievalToolbox; manual=false)
+    end
 
     @testset "Utils tests" begin
         include("utils_tests.jl")
