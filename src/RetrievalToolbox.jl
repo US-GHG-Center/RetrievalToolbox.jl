90bee4d78c90172f223679fc816c9ccc0696c759
Peter Somkuti <peter.somkuti@nasa.gov>
2025-11-13 13:54:42 -0500

    Fixed a typo.

--- src/RetrievalToolbox.jl
+++ src/RetrievalToolbox.jl
@@ -151,7 +151,7 @@ include("types/scene_types.jl")
 include("types/instrument_types.jl")
 include("types/radiance_types.jl")
 include("types/RT_types.jl")
-include("types/SIF_types.jl")
+include("types/sif_types.jl")
 
 include("types/inversion_types.jl")
 
