a378242c564effeffa9af567dd2c47a8037c57f2
Peter Somkuti <peter.somkuti@nasa.gov>
2025-11-21 10:53:04 -0500

    Fixed issue with a spectroscopy util test.

--- test/spectroscopy_tests.jl
+++ test/spectroscopy_tests.jl
@@ -4,6 +4,8 @@
     ww2 = rand(10); sort!(ww2)
     ## Remove all elements of ww2 that are lower than the smallest element of ww1
     ww2 = ww2[ww2 .> ww1[1]]
+    ## Remove all elements of ww2 that are larger than the largest element of ww1
+    ww2 = ww2[ww2 .< ww1[end]]
 
     result = searchsortedfirst.(Ref(ww1), ww2) .- 1
     @test all(RetrievalToolbox._find_ww_indices(ww1, ww2) .== result)
