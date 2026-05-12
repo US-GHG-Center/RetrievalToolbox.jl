41b2ad39d56c38988c1e0e98adb75e3bf988e3ba
PeterSomkuti <peter.somkuti@gmail.com>
2026-05-12 11:21:53 -0400

    Manual docstrings for aerosol types.

--- src/types/aerosol_types.jl
+++ src/types/aerosol_types.jl
@@ -1,76 +1,92 @@
 """
+    MieMomAerosolProperty{T<:Real} <: AbstractAerosolProperty
 
-    $(TYPEDFIELDS)
-    # Details
+# Fields
 
-    For now MOM files are expected to have all 6 phase function expansion coefficients,
-    regardless if further computations require the full phase matrix (polarization) or
-    not.
+- `ww::Vector{T}`: Wavelength or wavenumber at which the phase function is given
+- `ww_unit::Union{Unitful.WavenumberUnits, Unitful.LengthUnits}`: Unit of wavelength or
+  wavenumber
+- `Q_sca::Vector{T}`: Scattering efficiency per wavelength or wavenumber
+- `Q_ext::Vector{T}`: Extinction efficiency per wavelength or wavenumber
+- `omega::Vector{T}`: Single scatter albedo per wavelength or wavenumber
+- `max_coefs::Int`: Number of phase function expansion coefficients
+- `coefficients::Array{T, 3}`: Phase function expansion coefficients (coefficient, P
+  element, ww)
+- `work::Array{T, 3}`: Workspace for coefficient calculations (e.g. interpolation to some
+  wavelength)
+- `convention::NTuple{6, Symbol}`: Convention: which element in the phase matrix do
+  entries belong to?
 
-    The `convention` entry relates the 6 elements of the coefficient array to the
-    more meaningful elements of the phase matrix expansion coefficients. Expected values
-    are `(:a1, :a2, :a3, :a4, :b1, :b2)` or some permutation of those.
+# Details
+
+For now MOM files are expected to have all 6 phase function expansion coefficients,
+regardless if further computations require the full phase matrix (polarization) or
+not.
+
+The `convention` entry relates the 6 elements of the coefficient array to the
+more meaningful elements of the phase matrix expansion coefficients. Expected values
+are `(:a1, :a2, :a3, :a4, :b1, :b2)` or some permutation of those.
 
 """
-struct MieMomAerosolProperty{T} <: AbstractAerosolProperty
-    
-    "Wavelength or wavenumber at which the phase function is given"
+struct MieMomAerosolProperty{T<:Real} <: AbstractAerosolProperty
+
     ww::Vector{T}
-    "Unit of wavelength or wavenumber"
     ww_unit::Union{Unitful.WavenumberUnits, Unitful.LengthUnits}
-    
-    "Scattering efficiency per wavelength or wavenumber"
+
     Q_sca::Vector{T}
-    "Extinction efficiency per wavelength or wavenumber"
     Q_ext::Vector{T}
-    "Single scatter albedo per wavelength or wavenumber"
     omega::Vector{T}
-    "Number of phase function expansion coefficients"
     max_coefs::Int
-    "Phase function expansion coefficients (coefficient, P element, ww)"
     coefficients::Array{T, 3}
-    "Workspace for coefficient calculations (e.g. interpolation to some wavelength)"
     work::Array{T, 3}
-    "Convention: which element in the phase matrix do entries belong to?"
     convention::NTuple{6, Symbol}
 
 end
 
-struct HGAerosolProperty{T} <: AbstractAerosolProperty
 
-    g_value::T
+"""
+    GaussAerosol{T <: Real} <: AbstractAerosolType
 
-end
+Represents an aerosol that is vertically distributed along the (vertical) pressure axis
+according to a Gaussian distribution around a central pressure and some width. Functions
+down the line interpret the `total_optical_depth` as the full integrated optical depth
+from the surface pressure up to the top of the atmosphere.
 
+# Fields
 
-mutable struct GaussAerosol{T} <: AbstractAerosolType
+- `aerosol_name::String`: Aerosol name
+- `optical_property::AbstractAerosolProperty`: Optical properties of this aerosol
+- `relative_pressure::Bool`: If this aerosol layer pressure and width is defined as a
+  fraction of surface pressure
+- `pressure::T`: Center of the distribution in pressure units
+- `width::T`: Width in pressure units (equal to σ of a Gaussian)
+- `pressure_unit::Union{Unitful.PressureUnits, Unitful.DimensionlessUnits}`:
+- `ww_reference::T`: Pressure unit, or dimensionless unit if coordinates are relative to
+  surface pressure
+- `ww_unit::Union{Unitful.WavenumberUnits, Unitful.LengthUnits}`: Wavelength/wavenumber unit
+  wavelength/wavenumber at which the total optical depth is specified
+- `total_optical_depth::T`: Total-column optical depth of the distribution at valid
+  pressure coordinates, and the reference wavelength
 
-    "Aerosol name"
+"""
+mutable struct GaussAerosol{T <: Real} <: AbstractAerosolType
+
     aerosol_name::String
-    "Optical properties of this aerosol"
     optical_property::AbstractAerosolProperty
-    "If this aerosol layer pressure and width is defined as a fraction of surface pressure"
     relative_pressure::Bool
-    "Center of the distribution in pressure units"
     pressure::T
-    "Width in pressure units (equal to σ of a Gaussian)"
     width::T
-    "Pressure unit, or dimensionless unit if coordinates are relative to psurf"
     pressure_unit::Union{Unitful.PressureUnits, Unitful.DimensionlessUnits}
-    # Reference wavelength/wavenumber at which the total optical depth is specified
     ww_reference::T
-    # Wavelength/wavenumber unit
     ww_unit::Union{Unitful.WavenumberUnits, Unitful.LengthUnits}
-    "Total-column optical depth of the distribution at valid pressure coordinates, 
-     and the reference wavelength"
     total_optical_depth::T
 
 end
 
 """
-Pretty printing for spectral window types
-
 $(TYPEDSIGNATURES)
+
+Pretty printing for spectral window types
 """
 function show(io::IO, ::MIME"text/plain", aer::GaussAerosol)
 
@@ -79,9 +95,9 @@ function show(io::IO, ::MIME"text/plain", aer::GaussAerosol)
 end
 
 """
-Brief pretty printing for spectral window types
-
 $(TYPEDSIGNATURES)
+
+Brief pretty printing for spectral window types
 """
 function show(io::IO, aer::GaussAerosol)
 
@@ -89,11 +105,22 @@ function show(io::IO, aer::GaussAerosol)
 
 end
 
-
+"""
+Not implemented yet.
+"""
 struct TriangleAerosol{T} <: AbstractAerosolType
 
     optical_property::AbstractAerosolProperty
     height_level::T
     total_optical_depth::T
+
+end
+
+"""
+Not implemented yet.
+"""
+struct HGAerosolProperty{T} <: AbstractAerosolProperty
+
+    g_value::T
 
 end
